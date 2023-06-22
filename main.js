1f49e9aedda9556e5edcd2b76adcf0205c598e72
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-06-22 22:23:31 +0500

    Update

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
 // right animation
 
-TweenMax.to(
-    '#right', 2.5, {
+let tl = gsap.timeline();
+
+tl
+
+.to(
+    '#right', 2, {
     delay: .2,
     right: '-100%',
     ease: Expo.easeInOut,
     stragger: 1
-});
+})
 
-// left animation
-
-TweenMax.to(
-    '#left', 2, {
-    delay: .2,
-    left: '-100%',
-    ease: Expo.easeInOut,
-    stragger: 2 
-});
-
-// bottom animation
-
-TweenMax.to(
-    '#top', 2, {
-    delay: .2,
-    bottom: '-100%',
-    ease: Expo.easeInOut,
-    stragger: 3
-});
-
-// top animation
-
-TweenMax.to(
-    '#bottom', 2, {
-    delay: .2,
-    top: '-100%',
-    ease: Expo.easeInOut,
-    stragger: 4
-});
-
-// text animation
-
-TweenMax.from(
+.from(
     'h1', 1.5, {
     delay: .6,
     y: 50,
 }
-);
+)
+
+// left animation
+
+.to(
+    '#left', 1, {
+    delay: .2,
+    left: '-100%',
+    ease: Expo.easeInOut,
+    stragger: 2 
+})
+
+// bottom animation
+
+.to(
+    '#top', 1, {
+    delay: .2,
+    bottom: '-100%',
+    ease: Expo.easeInOut,
+    stragger: 3
+})
+
+// top animation
+
+.to(
+    '#bottom', 1, {
+    delay: .2,
+    top: '-100%',
+    ease: Expo.easeInOut,
+    stragger: 4
+})
+
+// text animation
+
